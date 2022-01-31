7c89a3d0e4796a7c009c55f2031d63631908be73
tomike19 <kolajoelizabeth@gmail.com>
2022-01-30 19:14:45 -0800

    working on the input field

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-var PropertyInput = document.getElementById('property-address');
-
-document.querySelector('form.form').addEventListener('submit', function (e) {
-
-    //prevent the normal submission of the form
-    e.preventDefault();
-
-    console.log(PropertyInput.value);    
+const $form = document.querySelector('.add-property-form');
+$form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData($form);
+  formData.forEach((value) => {
+    console.log( value);
+  });
 });
+
+
+let form = document.forms[0];
+let selectbox = form.selectbox;
+
+//console.log(selectbox);
+
+selectbox.addEventListener('change', ()=> {
+   console.log(selectbox.value);
+}, false);
